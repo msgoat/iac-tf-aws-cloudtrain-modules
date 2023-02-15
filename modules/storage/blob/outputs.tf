fa0f16059fc188201b395bd524f5a30eb8498f97
Michael Theis (msg) <michael.theis@msg.group>
2023-02-15 18:58:49 +0100

    added output s3_bucket_arn to module storage/blob

--- modules/storage/blob/outputs.tf
+++ modules/storage/blob/outputs.tf
@@ -3,6 +3,11 @@ output s3_bucket_id {
   value = aws_s3_bucket.blob.id
 }
 
+output s3_bucket_arn {
+  description = "Amazon resource name of the S3 bucket providing blob storage as used in policies"
+  value = aws_s3_bucket.blob.bucket
+}
+
 output s3_bucket_name {
   description = "Fully qualified name of the S3 bucket providing blob storage"
   value = aws_s3_bucket.blob.bucket
