4d09a03b4b56726f966a1af4b5566f52f5e8facf
Michael Theis (msg) <michael.theis@msg.group>
2023-03-03 12:55:19 +0100

    database/postgresql/rds: randomized secret name for postgres to avoid name collisions with previously deleted secrets

--- modules/database/postgresql/rds/secrets_manager_secret.tf
+++ modules/database/postgresql/rds/secrets_manager_secret.tf
@@ -1,8 +1,12 @@
+locals {
+  secret_name = "${local.db_instance_name}-${random_uuid.postgres.result}"
+}
+
 # create a SecretsManager secret to hold postgres username and password
 resource aws_secretsmanager_secret postgres {
-  name = local.db_instance_name
+  name = local.secret_name
   kms_key_id = aws_kms_key.cmk.arn
-  tags = merge({ Name = local.db_instance_name }, local.module_common_tags)
+  tags = merge({ Name = local.secret_name }, local.module_common_tags)
 }
 
 locals {
@@ -16,4 +20,8 @@ locals {
 resource aws_secretsmanager_secret_version postgres {
   secret_id = aws_secretsmanager_secret.postgres.id
   secret_string = jsonencode(local.secret_value)
+}
+
+resource random_uuid postgres {
+
 }
