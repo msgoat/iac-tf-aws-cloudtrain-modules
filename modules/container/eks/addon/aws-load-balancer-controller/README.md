3e4918d01ebbb3acd7b8fc4e80023b36623c0583
Michael Theis (msg) <michael.theis@msg.group>
2023-03-30 16:55:53 +0200

    container/eks/addon/aws-load-balancer-controller: added module

---
+++ modules/container/eks/addon/aws-load-balancer-controller/README.md
@@ -0,0 +1,24 @@
+# container/eks/addon/aws-load-balancer-controller 
+
+Terraform module to install the [AWS Load Balancer Controller](https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html) on any given AWS EKS cluster.
+
+By default, the AWS Load Balancer Controller is installed in Kubernetes namespace `aws-system` running with `2` replicas.
+
+## Prerequisites
+
+* Since the AWS Load Balancer Controller relies on [IAM roles for Kubernetes service accounts](https://docs.aws.amazon.com/eks/latest/userguide/iam-roles-for-service-accounts.html), 
+  an IAM OpenID Connect Provider must be present for the given EKS cluster.
+
+## Input Variables
+
+see: [variables.tf](variables.tf)
+
+## Outputs
+
+see: [outputs.tf](outputs.tf)
+
+## References
+
+@see [Installing the AWS Load Balancer Controller add-on](https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html)
+
+@see [AWS Load Balancer Controller on GitHub](https://github.com/kubernetes-sigs/aws-load-balancer-controller)
---
+++ modules/container/eks/addon/aws-load-balancer-controller/variables.tf
@@ -0,0 +1,58 @@
+# variables.tf
+# ---------------------------------------------------------------------------
+# Defines all input variable for this demo.
+# ---------------------------------------------------------------------------
+
+variable "region_name" {
+  description = "The AWS region to deploy into (e.g. eu-central-1)."
+  type        = string
+}
+
+variable "solution_name" {
+  description = "The name of the AWS solution that owns all AWS resources."
+  type        = string
+}
+
+variable "solution_stage" {
+  description = "The name of the current AWS solution stage."
+  type        = string
+}
+
+variable "solution_fqn" {
+  description = "The fully qualified name of the current AWS solution."
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags to be attached to all AWS resources"
+  type        = map(string)
+}
+
+variable "eks_cluster_name" {
+  description = "Name of the target AWS EKS cluster"
+  type        = string
+}
+
+variable "kubernetes_namespace_name" {
+  description = "Name of the Kubernetes namespace which should host the AWS Load Balancer Controller"
+  type = string
+  default = "aws-system"
+}
+
+variable "helm_release_name" {
+  description = "Name of the Helm release"
+  type = string
+  default = "aws-load-balancer-controller"
+}
+
+variable "helm_chart_version" {
+  description = "Version of the Helm chart"
+  type = string
+  default = "1.4.7"
+}
+
+variable "replica_count" {
+  description = "Number of replicas to run"
+  type = number
+  default = 2
+}
