9f457b5c20ff6b982d54db941c9a0d3e0dd11e16
Michael Theis (msg) <michael.theis@msg.group>
2023-05-17 11:05:12 +0200

    container/eks/tools: added ECK operator

--- modules/container/eks/tools/main.tf
+++ modules/container/eks/tools/main.tf
@@ -13,3 +13,12 @@ module monitoring {
   grafana_host_name = var.domain_name
   grafana_path = "/grafana"
 }
+
+module eck_operator {
+  source = "../tool/eck-operator"
+  region_name = var.region_name
+  solution_fqn = var.solution_fqn
+  solution_name = var.solution_name
+  solution_stage = var.solution_stage
+  common_tags = var.common_tags
+}
