36e4b955dd5725873153c360fd8ae7c64ae707f0
Michael Theis (msg) <michael.theis@msg.group>
2023-05-17 13:22:51 +0200

    container/eks/tools: added logging stack

--- modules/container/eks/tools/main.tf
+++ modules/container/eks/tools/main.tf
@@ -22,3 +22,16 @@ module eck_operator {
   solution_stage = var.solution_stage
   common_tags = var.common_tags
 }
+
+module logging {
+  source = "../tool/logging/efk-eck-operator"
+  region_name = var.region_name
+  solution_fqn = var.solution_fqn
+  solution_name = var.solution_name
+  solution_stage = var.solution_stage
+  common_tags = var.common_tags
+  eks_cluster_name = var.eks_cluster_name
+  kibana_host_name = var.domain_name
+  kibana_path = "/kibana"
+  depends_on = [ module.eck_operator ]
+}
