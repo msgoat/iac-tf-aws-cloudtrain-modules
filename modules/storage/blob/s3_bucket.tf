f61f2a0eb928ea8c2e55d3cd68fdb44e975d1c55
saackef <fabian.saacke@msg.group>
2023-11-17 07:57:17 +0100

    Extend the S3 bucket module

--- modules/storage/blob/s3_bucket.tf
+++ modules/storage/blob/s3_bucket.tf
@@ -5,12 +5,12 @@ locals {
 // create s3 bucket as container for blobs
 resource aws_s3_bucket blob {
   bucket = local.s3_bucket_name
-  tags = merge({Name = local.s3_bucket_name}, local.module_common_tags)
+  tags   = merge({ Name = local.s3_bucket_name }, local.module_common_tags)
 }
 
 // enable versioning on S3 bucket to be able to recover from unintended state changes
 resource aws_s3_bucket_versioning blob {
-  count = var.versioning_enabled ? 1 : 0
+  count  = var.versioning_enabled ? 1 : 0
   bucket = aws_s3_bucket.blob.id
   versioning_configuration {
     status = "Enabled"
@@ -19,10 +19,10 @@ resource aws_s3_bucket_versioning blob {
 
 // block public access to bucket
 resource aws_s3_bucket_public_access_block block_public_access {
-  count = var.public_access_enabled ? 0 : 1
-  bucket = aws_s3_bucket.blob.id
-  block_public_acls   = true
-  block_public_policy = true
+  count                   = var.public_access_enabled ? 0 : 1
+  bucket                  = aws_s3_bucket.blob.id
+  block_public_acls       = true
+  block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
@@ -32,25 +32,35 @@ resource aws_s3_bucket_server_side_encryption_configuration blob {
   bucket = aws_s3_bucket.blob.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      kms_master_key_id = var.custom_encryption_kms_key_arn
+      sse_algorithm     = "aws:kms"
     }
     bucket_key_enabled = true
   }
+}
+
+// optionally enable logging to a target bucket
+resource aws_s3_bucket_logging main {
+  count  = var.logging_bucket_name != null ? 1 : 0
+  bucket = aws_s3_bucket.blob.id
+
+  target_bucket = var.logging_bucket_name
+  target_prefix = "bucket/${aws_s3_bucket.blob.bucket}"
 }
 
 // add a bucket policy which denies HTTP access
 resource aws_s3_bucket_policy deny_http_access {
   bucket = aws_s3_bucket.blob.id
   policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "BUCKET-POLICY"
+    Version   = "2012-10-17"
+    Id        = "BUCKET-POLICY"
     Statement = [
       {
         Sid       = "EnforceTls"
         Effect    = "Deny"
         Principal = "*"
         Action    = "s3:*"
-        Resource = [
+        Resource  = [
           "${aws_s3_bucket.blob.arn}/*",
           "${aws_s3_bucket.blob.arn}",
         ]
@@ -59,7 +69,63 @@ resource aws_s3_bucket_policy deny_http_access {
             "aws:SecureTransport" = "false"
           }
           NumericLessThan = {
-            "s3:TlsVersion": 1.2
+            "s3:TlsVersion" : 1.2
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource aws_s3_bucket_policy Deny_cross_account_access {
+  bucket = aws_s3_bucket.blob.id
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Id        = "BUCKET-POLICY"
+    Statement = [
+      {
+        Sid       = "DenyUnencryptedObjectUploads"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource  = [
+          "${aws_s3_bucket.blob.arn}/*",
+          "${aws_s3_bucket.blob.arn}",
+        ]
+        Condition = {
+          StringNotEquals = {
+            "aws:PrincipalAccount" = [data.aws_caller_identity.current.account_id]
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource aws_s3_bucket_policy deny_unencrypted_uploads {
+  count  = var.deny_unencrypted_uploads == true ? 1 : 0
+  bucket = aws_s3_bucket.blob.id
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Id        = "BUCKET-POLICY"
+    Statement = [
+      {
+        Sid       = "DenyUnencryptedObjectUploads"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:PutObject"
+        Resource  = [
+          "${aws_s3_bucket.blob.arn}/*",
+          "${aws_s3_bucket.blob.arn}",
+        ]
+        Condition = {
+          Null = {
+            "s3:x-amz-server-side-encryption" = "true"
+          }
+          StringNotEqualsIfExists = {
+            "s3:x-amz-server-side-encryption" = "aws:kms"
           }
         }
       },
--- modules/storage/blob/variables.tf
+++ modules/storage/blob/variables.tf
@@ -2,48 +2,59 @@
 
 variable region_name {
   description = "The AWS region to deploy into (e.g. eu-central-1)."
-  type = string
+  type        = string
 }
 
 variable solution_name {
   description = "The name of the solution that owns all AWS resources."
-  type = string
+  type        = string
 }
 
 variable solution_stage {
   description = "The name of the current environment stage."
-  type = string
+  type        = string
 }
 
 variable solution_fqn {
   description = "The fully qualified name of the solution (i.e. solution_name + solution_stage in most cases)."
-  type = string
+  type        = string
 }
 
 variable common_tags {
   description = "Common tags to be attached to all AWS resources."
-  type = map(string)
+  type        = map(string)
 }
 
 variable bucket_name {
   description = "Logical name of the S3 bucket; will be expanded to a fully qualified S3 bucket name"
-  type = string
+  type        = string
 }
 
 variable versioning_enabled {
   description = "Controls if blobs should be versioned; default false"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable public_access_enabled {
   description = "Controls if blobs should be accessible via internet; default false"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
-variable custom_encryption_enabled {
-  description = "Controls if blobs should be encrypted using a customer managed key in KMS; default false"
-  type = bool
-  default = false
+variable "logging_bucket_name" {
+  description = "Name for an optional logging bucket"
+  type        = string
+  default     = null
+}
+
+variable "custom_encryption_kms_key_arn" {
+  description = "ARN of the KMS key used for bucket encryption"
+  type        = string
+}
+
+variable "deny_unencrypted_uploads" {
+  description = "Attaches a S3 bucket policy to prevent unencrypted objects upload"
+  type        = bool
+  default     = true
 }
