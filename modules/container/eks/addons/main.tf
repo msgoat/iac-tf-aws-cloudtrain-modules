df861afc7e9c578c6faa2482cb7e1cfe10df5491
Michael Theis (msg) <michael.theis@msg.group>
2024-03-13 18:42:22 +0100

    Refactored container/eks/addons with prometheus support

--- modules/container/eks/addons/main.tf
+++ modules/container/eks/addons/main.tf
@@ -13,16 +13,8 @@ module aws_auth {
   eks_cluster_admin_role_names = var.eks_cluster_admin_role_names
 }
 
-module metrics_server {
-  source = "../addon/metrics-server"
-  region_name = var.region_name
-  solution_fqn = var.solution_fqn
-  solution_name = var.solution_name
-  solution_stage = var.solution_stage
-  common_tags = var.common_tags
-}
-
 module aws_ebs_csi_driver {
+  count = var.addon_aws_ebs_csi_driver_enabled ? 1 : 0
   source = "../addon/aws-ebs-csi-driver"
   region_name = var.region_name
   solution_fqn = var.solution_fqn
@@ -32,7 +24,18 @@ module aws_ebs_csi_driver {
   eks_cluster_name = var.eks_cluster_name
 }
 
+module metrics_server {
+  count = var.addon_metrics_server_enabled ? 1 : 0
+  source = "../addon/metrics-server"
+  region_name = var.region_name
+  solution_fqn = var.solution_fqn
+  solution_name = var.solution_name
+  solution_stage = var.solution_stage
+  common_tags = var.common_tags
+}
+
 module cluster_autoscaler {
+  count = var.addon_cluster_autoscaler_enabled ? 1 : 0
   source = "../addon/cluster-autoscaler"
   region_name = var.region_name
   solution_fqn = var.solution_fqn
@@ -42,7 +45,20 @@ module cluster_autoscaler {
   eks_cluster_name = var.eks_cluster_name
 }
 
+module prometheus {
+  count = var.addon_prometheus_enabled ? 1 : 0
+  source = "../addon/prometheus"
+  region_name = var.region_name
+  solution_fqn = var.solution_fqn
+  solution_name = var.solution_name
+  solution_stage = var.solution_stage
+  common_tags = var.common_tags
+  eks_cluster_name = var.eks_cluster_name
+  cert_manager_enabled = false
+}
+
 module cert_manager {
+  count = var.addon_cert_manager_enabled ? 1 : 0
   source = "../addon/cert-manager"
   region_name = var.region_name
   solution_fqn = var.solution_fqn
@@ -52,9 +68,11 @@ module cert_manager {
   eks_cluster_name = var.eks_cluster_name
   hosted_zone_name = var.hosted_zone_name
   letsencrypt_account_name = var.letsencrypt_account_name
+  prometheus_operator_enabled = var.addon_prometheus_enabled
 }
 
 module aws_load_balancer_controller {
+  count = var.addon_aws_load_balancer_controller_enabled ? 1 : 0
   source = "../addon/aws-load-balancer-controller"
   region_name = var.region_name
   solution_fqn = var.solution_fqn
@@ -62,6 +80,7 @@ module aws_load_balancer_controller {
   solution_stage = var.solution_stage
   common_tags = var.common_tags
   eks_cluster_name = var.eks_cluster_name
-  cert_manager_enabled = var.cert_manager_enabled
+  cert_manager_enabled = var.addon_cert_manager_enabled
   depends_on = [ module.cert_manager ]
+  prometheus_operator_enabled = var.addon_prometheus_enabled
 }
--- modules/container/eks/addons/variables.tf
+++ modules/container/eks/addons/variables.tf
@@ -52,3 +52,51 @@ variable eks_cluster_admin_role_names {
   description = "IAM role names to be added as system:masters to aws_auth config map"
   type = list(string)
 }
+
+variable "ensure_high_availability" {
+  description = "Controls if a high availability of this service should be ensured by running at least two pods spread across AZs and nodes"
+  type = bool
+  default = true
+}
+
+variable addon_aws_auth_enabled {
+  description = "Controls if addon `aws_auth` should be enabled; default `true`"
+  type = bool
+  default = true
+}
+
+variable addon_aws_ebs_csi_driver_enabled {
+  description = "Controls if addon `aws_ebs_csi_driver_enabled` should be enabled; default `true`"
+  type = bool
+  default = true
+}
+
+variable addon_metrics_server_enabled {
+  description = "Controls if addon `metrics_server` should be enabled; default `true`"
+  type = bool
+  default = true
+}
+
+variable addon_cluster_autoscaler_enabled {
+  description = "Controls if addon `cluster_autoscaler` should be enabled; default `true`"
+  type = bool
+  default = true
+}
+
+variable addon_prometheus_enabled {
+  description = "Controls if addon `prometheus` should be enabled; default `true`"
+  type = bool
+  default = true
+}
+
+variable addon_cert_manager_enabled {
+  description = "Controls if addon `cert_manager_enabled` should be enabled; default `true`"
+  type = bool
+  default = true
+}
+
+variable addon_aws_load_balancer_controller_enabled {
+  description = "Controls if addon `aws_load_balancer_controller` should be enabled; default `true`"
+  type = bool
+  default = true
+}
