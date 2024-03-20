b737e9029a326a3b9f1a6e859c8ae04e7765c0be
Michael Theis (msg) <michael.theis@msg.group>
2024-03-20 17:56:30 +0100

    removed obsolete module prometheus

--- modules/container/eks/addons/main.tf
+++ modules/container/eks/addons/main.tf
@@ -45,18 +45,6 @@ module "cluster_autoscaler" {
   eks_cluster_name = var.eks_cluster_name
 }
 
-module "prometheus" {
-  count                = var.addon_prometheus_enabled ? 1 : 0
-  source               = "../addon/prometheus"
-  region_name          = var.region_name
-  solution_fqn         = var.solution_fqn
-  solution_name        = var.solution_name
-  solution_stage       = var.solution_stage
-  common_tags          = var.common_tags
-  eks_cluster_name     = var.eks_cluster_name
-  cert_manager_enabled = false
-}
-
 module "cert_manager" {
   count                       = var.addon_cert_manager_enabled ? 1 : 0
   source                      = "../addon/cert-manager"
